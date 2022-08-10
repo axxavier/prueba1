a4c8bb6445f3551b569b89cce45d3b000d5dcb58
axxavier <xavielax32@gmail.com>
2022-08-10 03:57:34 +0200

    primer commit

---
+++ prub.py
@@ -0,0 +1,25 @@
+
+from itertools import count
+from re import A
+
+
+#print("calculo de Horas")
+print("")
+#nombre= input("Digite  ")
+
+#NUMERO1= int(input ("digiste la cantida de hora trabajada= "))
+
+#while  NUMERO1 == 0:
+  #  NUMERO1= int(input ("digiste la cantida de hora trabajada= "\n))
+#NUMERO2= int(input("digiste el precio por hora= "\n))
+
+#SUMA=NUMERO1 *  NUMERO2
+#print(nombre,"Tiene un total de  ",SUMA,"De hora trabajada")
+
+numeroSecreto = 777
+numero = int(input("Digite un numero entero"))
+
+while numero !=numeroSecreto:
+    
+    print("¡Ja, ja! ¡Estás atrapado en mi ciclo!")
+    numero = int(input("Digite un numero entero"))
