cf239b29b33eea136195736faf89d366ab559328
axxavier <xavielax32@gmail.com>
2022-08-16 04:12:15 +0200

    nuevo print

--- prub.py
+++ prub.py
@@ -1,10 +1,11 @@
 
 from itertools import count
 from re import A
+import time
 
 
 #print("calculo de Horas")
-print("")
+#print("")
 #nombre= input("Digite  ")
 
 #NUMERO1= int(input ("digiste la cantida de hora trabajada= "))
@@ -16,12 +17,31 @@ print("")
 #SUMA=NUMERO1 *  NUMERO2
 #print(nombre,"Tiene un total de  ",SUMA,"De hora trabajada")
 
-numeroSecreto = 777
-numero = int(input("Digite un numero entero"))
+#numeroSecreto = 777
+#numero = int(input("Digite un numero entero"))
 
-while numero !=numeroSecreto:
+#while numero !=numeroSecreto:
     
-    print("¡Ja, ja! ¡Estás atrapado en mi ciclo!")
-    numero = int(input("Digite un numero entero"))
+ #   print("¡Ja, ja! ¡Estás atrapado en mi ciclo!")
+  #  numero = int(input("Digite un numero entero"))
 
-print("usted Adivino la carta")
+#print("usted Adivino la carta")
+#for i in range (2, 9):
+ #   print("El valor de i es actualmente", i)
+
+  
+# Indicar al usuario que ingrese una palabra
+palabra =  input("Digite una palabra ")
+palabra = palabra.upper()
+# y asignarlo a la variable userWord.
+
+for letra in palabra:
+    if letra == "A" or letra == "E" or letra == "I" or  letra == "O"  or letra == "U":
+        continue
+    else:
+        time.sleep(1)
+
+    print(letra)
+    
+    
+    # Completa el cuerpo del ciclo for.j
